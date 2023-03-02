fef3dab15b190e7f9340304f5e0e1c5d30a80736
gsh <gsh@10.232.140.1>
2023-03-02 14:14:22 -0600

    Studio 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -16,7 +16,7 @@ public class Methods {
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 		return distance;
 	}
 
@@ -34,17 +34,21 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledCircle(x, y, ((radius*3)/4));
 
-		
+
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledCircle(x, y, radius/2);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		StdDraw.filledCircle(x, y, radius/4);
 		
 	}
 
@@ -62,8 +66,21 @@ public class Methods {
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
 		// TODO: Finish this method
-		
+		for(int i=0; i<source.length(); i++)
+		{
+			char c = charAt(i);
+			if (c == target)
+			{
+				result = result + replacement;
+			}
+			
+		}
 		return result;
+	}
+
+	private static void charAt(int i) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	/**
